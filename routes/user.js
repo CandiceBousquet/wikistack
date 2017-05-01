943571a0b309f3ec5333d04171fb2fc3ca875dbb
brianhong <brianhong0510@gmail.com>
2017-05-01 17:26:18 -0400

    Basic of single user page working

--- routes/user.js
+++ routes/user.js
@@ -2,19 +2,47 @@ const express = require('express');
 const router = express.Router();
 const model = require("../models");
 const User = model.User;
+const Page = model.Page;
+const Promise = require('bluebird');
 
 router.get('/', function(req, res, next) {
 	var users = User.findAll()
   .then(function(result) {
     res.render('./users.html', {users: result});
+  })
+  .catch(next);
+});
+
+router.get('/:id', function(req, res, next) {
+  // User lookup
+  var user = User.findOne({
+    where: {
+      id: req.params.id
+    }
   });
-  // allUsers.then(function(users){
-  //   var userArr = [];
-  //   for (var i = 0; i < users.length; i++) {
-  //     userArr.push(users[i]);
-  //   }
-  //   res.render('./users.html', userArr);
-  // });
+
+  // Page lookup whose author is this user
+  var pages = Page.findAll({
+    where: {
+      authorId: req.params.id
+    }
+  });
+
+  Promise.all([user, pages]).then(function(data) {
+    console.log(data[0]); // id, name, email
+    console.log(data[1]); // id, title, urlTitle, content, status, authorId
+
+    const localVars = {
+      email: data[0].email,
+      name: data[0].name,
+      pages: data[1]
+    };
+    // for(var i = 0; i < data.length; i++) {
+    //   localVars.push(data[i]);
+    // }
+    // console.log(localVars);
+    res.render('./userList.html', {user: localVars});
+  });
 });
 
 module.exports = router;
